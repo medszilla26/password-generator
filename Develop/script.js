8022a42edcf9c58322fe82503730532be7269d5a
M3D5 <pablohdzjr@gmail.com>
2020-04-26 20:25:09 -0500

    added a please try again alert

--- Develop/script.js
+++ Develop/script.js
@@ -19,18 +19,16 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   var lengthConfirmed = prompt(
-    "Please enter the amount of characters you would like in your password:"
+    "Please enter the amount of characters you would like in your password:(Password must be between 10-128 characters in length)"
   );
-
-  if (lengthConfirmed < 9 || lengthConfirmed > 128);
+  if (lengthConfirmed <= 9 || lengthConfirmed >= 129) {
+    alert(
+      "Password must be between 10 to 128 character long, please try again!"
+    );
+  }
 
   var uppercaseConfirmed = confirm(" Would you like to include uppercase?");
   var lowercaseConfirmed = confirm(" Would you like to include lowercase?");
   var symbolsConfirmed = confirm(" Would you like to include symbols?");
   var numbersConfirmed = confirm(" Would you like to include numbers?");
-}
-if (confirm("Press a button!")) {
-  console.log("You pressed OK!");
-} else {
-  console.log("You pressed Cancel!");
 }
